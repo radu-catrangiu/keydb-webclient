551740ea020e533d070d6f98fdab8d2f3fc41903
Radu Catrangiu <radu.catrangiu@gmail.com>
2023-10-28 21:59:55 +0300

    Update paths.

--- src/routes/connection/[slug]/+layout.svelte
+++ src/routes/connection/[slug]/+layout.svelte
@@ -1,14 +1,14 @@
 <script>
-    import { page } from '$app/stores'
-    const basePath = $page.url.pathname;
+    // import { page } from '$app/stores'
+    // const basePath = $page.url.pathname;
 </script>
 <div class="row mt-3">
     <div class="col-2">
         <div class="d-grid gap-2">
-            <a class="btn btn-primary" type="button" href="{basePath}/">Details</a>
-            <a class="btn btn-primary" type="button" href="{basePath}/browser/">Browser</a>
-            <a class="btn btn-primary" type="button" href="{basePath}/cli/">CLI</a>
-            <a class="btn btn-primary" type="button" href="{basePath}/clients-list/">Clients List</a>
+            <a class="btn btn-primary" type="button" href="./">Details</a>
+            <a class="btn btn-primary" type="button" href="./browser/">Browser</a>
+            <a class="btn btn-primary" type="button" href="./cli/">CLI</a>
+            <a class="btn btn-primary" type="button" href="./clients-list/">Clients List</a>
         </div>
     </div>
     <div class="col mx-5">
