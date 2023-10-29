ac21cd58843c8c7b495d86c94b5e7c5d32e6ba20
Radu Catrangiu <radu.catrangiu@gmail.com>
2023-10-29 15:22:22 +0200

    Add multiResponse handler.

--- src/lib/server/commandRunner.js
+++ src/lib/server/commandRunner.js
@@ -2,9 +2,16 @@ import { error } from "@sveltejs/kit";
 import { getConnection } from "./database";
 
 /**
+ * @typedef RunCommandResult
+ * @property {[Error | null, any]} selectResult
+ * @property {[Error | null, any]} commandResult
+ */
+
+/**
  * @param {string} slug
  * @param {number} db
  * @param {string} command
+ * @returns {Promise<RunCommandResult>}
  */
 export async function runCommand(slug, db, command) {
     let connectionDetails;
@@ -51,4 +58,21 @@ export async function runCommand(slug, db, command) {
         selectResult,
         commandResult,
     };
+}
+
+/**
+ * @param {RunCommandResult} runCommandResult
+ */
+export function handleMultiResponse(runCommandResult) {
+    if (runCommandResult.selectResult[0] !== null) {
+        const err = runCommandResult.selectResult[0];
+        throw error(500, err);
+    }
+
+    if (runCommandResult.commandResult[0] !== null) {
+        const err = runCommandResult.commandResult[0];
+        throw error(500, err);
+    }
+
+    return runCommandResult.commandResult[1];
 }
