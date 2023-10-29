3d681b1505b58183320479d44169629384e3192c
Radu Catrangiu <radu.catrangiu@gmail.com>
2023-10-29 15:22:55 +0200

    Update list handler to user handleMultiResponse func.

--- src/routes/connection/[slug]/browser/list/+server.js
+++ src/routes/connection/[slug]/browser/list/+server.js
@@ -1,7 +1,6 @@
-import { runCommand } from '$lib/server/commandRunner';
+import { handleMultiResponse, runCommand } from '$lib/server/commandRunner';
 import { getQuery, getSlug } from '$lib/server/paramsHelper';
 import { json } from '@sveltejs/kit';
-import { error } from 'console';
 
 /**
  * @typedef RawListQueryParams
@@ -24,20 +23,11 @@ export async function GET(event) {
 
     const command = `SCAN ${query.cursor || 0} MATCH ${pattern} COUNT ${query.count || 10}`
 
-    const result = await runCommand(slug, db, command)
+    const response = await runCommand(slug, db, command)
 
-    if (result.selectResult[0] !== null) {
-        const err = result.selectResult[0];
-        throw error(err.message);
-    }
+    const result = handleMultiResponse(response);
 
-    if (result.commandResult[0] !== null) {
-        const err = result.commandResult[0];
-        throw error(err.message);
-    }
-
-    const [newCursor, list] = result.commandResult[1];
-
+    const [newCursor, list] = result;
 
     return json({
         finish: newCursor === "0",
