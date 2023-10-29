a7bb8a95892170b396185a4744f70b724bf1a611
Radu Catrangiu <radu.catrangiu@gmail.com>
2023-10-29 22:21:12 +0200

    Update connections page.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-    import NewConnectionModal from '$lib/modals/NewConnectionModal.svelte'
+    import NewConnectionModal from "$lib/modals/NewConnectionModal.svelte";
     /**
      * @type {import("./+page.server").ConnectionsPageResponse}
      */
@@ -7,25 +7,62 @@
 
     let showNewConnectionModal = false;
 
-	const onCancelClick = () => {
-		showNewConnectionModal = true;
-	}
-
-	const onModalClose = () => {
-		showNewConnectionModal = false;
-	}
+    const onModalClose = () => {
+        showNewConnectionModal = false;
+    };
 </script>
 
-<h1>Welcome to KeyDB Web Client</h1>
 
-<button type="button" class="btn btn-primary" on:click={() => showNewConnectionModal=true}>Add Keydb Database</button>
 
-<NewConnectionModal open={showNewConnectionModal} onClosed={() => onModalClose()}/>
-
-<div class="my-3">
-    <div class="d-grid gap-2">
+<div class="container my-3">
+    <div class="row my-5">
+        <h1 class="text-center">Welcome to KeyDB Web Client</h1>
+    </div>
+    <div class="row my-3">
+        <div class="text-center">
+            <button
+                style="width: 18rem;"
+                type="button"
+                class="btn btn-primary"
+                on:click={() => (showNewConnectionModal = true)}>Add Keydb Database</button
+            >
+        </div>
+    </div>
+    <div class="row row-cols-2">
         {#each data.connections as connection}
-            <a class="btn btn-primary" href="/connection/{connection.slug}/info" type="button">{connection.label}</a>
+            <div class="col">
+                <div class="card text-center m-2" style="">
+                    <div class="card-body">
+                        <h5 class="card-title">{connection.label}</h5>
+                        
+                            <table class="table">
+                                <tbody>
+                                    <tr>
+                                        <th>Host</th>
+                                        <td>{connection.host}</td>
+                                    </tr>
+                                    <tr>
+                                        <th>Port</th>
+                                        <td>{connection.port}</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        
+                        <div class="d-grid gap-2">
+                            <a
+                            class="btn btn-primary"
+                            href="/connection/{connection.slug}/info"
+                            type="button">Open <b>{connection.label}</b></a
+                            >
+                        </div>
+                    </div>
+                </div>
+            </div>
         {/each}
     </div>
 </div>
+
+<NewConnectionModal
+    open={showNewConnectionModal}
+    onClosed={() => onModalClose()}
+/>
